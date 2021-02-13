3238fd763d11b020090c61268befc03df77d1c7d
otaon <16165624+otaon@users.noreply.github.com>
2021-02-13 19:12:54 +0900

    edit description

--- puzzle/CodeIQ/極めよプログラミング道！【実力判定：Aランク】/question.py
+++ puzzle/CodeIQ/極めよプログラミング道！【実力判定：Aランク】/question.py
@@ -1,17 +1,20 @@
-dataset = []
+Nset = []
 try:
 	while True:
-		dataset.append(int(input().strip().upper()))
+		Nset.append(int(input().strip().upper()))
 except EOFError:
 	pass
 
-MAX = 1999999
-for data in dataset:
-	if data % 2 == 0:
+SUM_MAX = 1999999	# "連続した2値の合計"の最大値
+for N in Nset:
+	if N % 2 == 0:
+		# ある整数Nが偶数の場合、連続した2値がNの倍数となる組み合わせは0個で確定
 		print(0)
 	else:
-		num_N = MAX // data
-		if num_N % 2 == 0:
-			print(num_N // 2)
+		# ある整数Nが奇数の場合、Nの倍数は偶奇を繰り返す
+		# そのうち、奇数であるもののみ"連続した2値の合計"に含まれる
+		count = SUM_MAX // N
+		if count % 2 == 0:
+			print(count // 2)
 		else:
-			print(num_N // 2 + 1)
+			print(count // 2 + 1)
